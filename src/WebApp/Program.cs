1aa80e28de789321d28783bcd9a06da679898c0c
QuocDuong <65280368+Duongdot@users.noreply.github.com>
2024-11-22 01:52:21 +0700

    Update Program.cs
    develop

--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -16,9 +16,8 @@ app.MapDefaultEndpoints();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    //develop
     app.UseExceptionHandler("/Error");
-    //Duong
-    // develop
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
