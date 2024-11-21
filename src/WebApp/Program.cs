92cdd615fcc41e79f7c5b964993cbbf870a6f99d
QuocDuong <65280368+Duongdot@users.noreply.github.com>
2024-11-22 01:12:02 +0700

    Update Program.cs
    comment Duong

--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -17,6 +17,7 @@ app.MapDefaultEndpoints();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
+    //Duong
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
